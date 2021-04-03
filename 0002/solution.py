f56f175194a4020344139f1b8dd14e4d6652eb90
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-04-03 15:08:57 -0300

    ci: add solution.py

---
+++ 0002/solution.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def fib(bar: int) -> int:
+    if bar < 2:
+        return bar
+    return fib(bar-1) + fib(bar-2)
+
+def solve(threshold: int) -> (int):
+    solution, auxiliar = 0, 0
+    i = 1
+
+    auxiliar = fib(i)
+    while auxiliar < threshold:
+        if (auxiliar % 2 == 0):
+            solution += auxiliar
+        i=i+1
+        auxiliar = fib(i)
+
+    return solution
+
+if __name__ == '__main__':
+    print(solve(4000000))
